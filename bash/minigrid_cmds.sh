f2ec8aa984d4cee87b4f243026b17db4b0f2e506
Ben <522181941@qq.com>
2022-09-14 19:48:07 +0800

    tmp

--- bash/minigrid_cmds.sh
+++ bash/minigrid_cmds.sh
@@ -1,3 +1,4 @@
 pip install gym==0.25.2
 
 python dreamerv2/train.py --logdir ./data/minigrid/AC --configs minigrid --task minigrid_MiniGrid-DoorKey-6x6-v0
+python dreamerv2/train.py --logdir ./data/minigrid/MTCS --configs minigrid mcts --task minigrid_MiniGrid-DoorKey-6x6-v0
