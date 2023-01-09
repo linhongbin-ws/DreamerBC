d725c4bd979a8c95f780a29be8ab7c6e3bfbab27
Ben <522181941@qq.com>
2023-01-09 21:15:00 +0800

    uncomment fix

--- examples/suture_json.py
+++ examples/suture_json.py
@@ -47,63 +47,63 @@ config = config.update({
   'seed': args.seed,
                  })
 
-# assert args.baseline in ["DreamerBC", "Dreamer"]
-# if args.baseline == "Dreamer":
-#   config=config.update({
-#     "bc_grad_weight": 0,
-#     "bc_wm_retrain": False,
-#     "bc_agent_retrain": False,
-#   })
+assert args.baseline in ["DreamerBC", "Dreamer"]
+if args.baseline == "Dreamer":
+  config=config.update({
+    "bc_grad_weight": 0,
+    "bc_wm_retrain": False,
+    "bc_agent_retrain": False,
+  })
 
-# if args.prefill>=0:
-#   config=config.update({
-#     "prefill": args.prefill
-#   })
-# print(config)
+if args.prefill>=0:
+  config=config.update({
+    "prefill": args.prefill
+  })
+print(config)
 
-# # if config.is_pure_train:
-# #   env = None
-# # else:
-# # env = make_env('ambf_needle_picking_64x64_discrete',  
-# #                     is_visualizer=True, 
-# #             image_preprocess_type=image_preprocess_type,
-# #                image_preprocess_method=args.seg_method,
-# #             is_depth=True, 
-# #             is_idle_action=False,
-# #             is_ds4_oracle=False,
-# #             action_arm_device='psm2',
-# #             obs_type="image",
-# #             timelimit=None,
-# #             resize_resolution=64,
-# #             is_dummy=config.is_pure_train,
+# if config.is_pure_train:
+#   env = None
+# else:
+# env = make_env('ambf_needle_picking_64x64_discrete',  
+#                     is_visualizer=True, 
+#             image_preprocess_type=image_preprocess_type,
+#                image_preprocess_method=args.seg_method,
+#             is_depth=True, 
+#             is_idle_action=False,
+#             is_ds4_oracle=False,
+#             action_arm_device='psm2',
+#             obs_type="image",
+#             timelimit=None,
+#             resize_resolution=64,
+#             is_dummy=config.is_pure_train,
             
-# env = make_env(task="{}_needle_picking_64x64_discrete".format(args.robot),
-#              image_preprocess_type=args.preprocess_type if args.preprocess_type not in ["origin"] else None, 
-#              image_preprocess_method=args.preprocess_method,
-#             #  scalar2image_obs_key=["gripper_state", "state"],
-#              action_arm_device=args.arm,
-#              clutch_start_engaged=args.clutch,
-#              resize_resolution=64,
-#             #  timelimit=-1, 
-#             #  is_depth=True, 
-#             #  is_idle_action=False,
-#              is_ds4_oracle=False,
-#              is_visualizer=False,
-#             #  is_visualizer_blocking=True, 
-#              is_dummy=config.is_pure_train,
-# )
-# env.seed = args.seed
-# # # obs = env.reset()
-# # # import cv2
-# # # frame = cv2.resize(obs['image'], (1080, 1080), interpolation=cv2.INTER_AREA)
-# # # cv2.imshow('preview', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)) # Display the resulting frame
-# # # k = cv2.waitKey(0)
-# # # for i in range(4):
-# # #   obs, reward, done, info = env.step(env.action_space.sample())
-# # #   frame = cv2.resize(obs['image'], (1080, 1080), interpolation=cv2.INTER_AREA)
-# # #   cv2.imshow('preview', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)) # Display the resulting frame
-# # #   k = cv2.waitKey(0)
-# dv2.train(env, config, is_pure_train=args.only_train, is_pure_datagen=args.only_datagen)
-# # print(env.observation_space)
-# # print(env.action_space)
-# env.close()
+env = make_env(task="{}_needle_picking_64x64_discrete".format(args.robot),
+             image_preprocess_type=args.preprocess_type if args.preprocess_type not in ["origin"] else None, 
+             image_preprocess_method=args.preprocess_method,
+            #  scalar2image_obs_key=["gripper_state", "state"],
+             action_arm_device=args.arm,
+             clutch_start_engaged=args.clutch,
+             resize_resolution=64,
+            #  timelimit=-1, 
+            #  is_depth=True, 
+            #  is_idle_action=False,
+             is_ds4_oracle=False,
+             is_visualizer=False,
+            #  is_visualizer_blocking=True, 
+             is_dummy=config.is_pure_train,
+)
+env.seed = args.seed
+# # obs = env.reset()
+# # import cv2
+# # frame = cv2.resize(obs['image'], (1080, 1080), interpolation=cv2.INTER_AREA)
+# # cv2.imshow('preview', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)) # Display the resulting frame
+# # k = cv2.waitKey(0)
+# # for i in range(4):
+# #   obs, reward, done, info = env.step(env.action_space.sample())
+# #   frame = cv2.resize(obs['image'], (1080, 1080), interpolation=cv2.INTER_AREA)
+# #   cv2.imshow('preview', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)) # Display the resulting frame
+# #   k = cv2.waitKey(0)
+dv2.train(env, config, is_pure_train=args.only_train, is_pure_datagen=args.only_datagen)
+# print(env.observation_space)
+# print(env.action_space)
+env.close()
